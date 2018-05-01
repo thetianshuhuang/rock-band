fd641be30da0be8114c93584bcf0916d5552bdd4
theqb <31521302+theqb@users.noreply.github.com>
2018-05-01 00:42:59 -0500

    moved guitar to its own folder

---
+++ display/guitar.h
@@ -0,0 +1,19 @@
+/*
+ * guitar.h
+ *
+ * Graphics for the onscreen guitar
+ */
+ 
+#ifndef GUITAR_H
+#define GUITAR_H
+
+#include <stdint.h>
+
+// ----------drawGuitar----------
+// initialize guitar graphics
+// Parameters
+//      none
+void drawGuitar(void);
+
+
+#endif
