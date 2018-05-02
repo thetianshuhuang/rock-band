3ceae14a57fde6f0b27b12ac205e777474b6559c
theqb <31521302+theqb@users.noreply.github.com>
2018-05-02 01:21:55 -0500

    minor tweaks

--- game/core.c
+++ game/core.c
@@ -108,9 +108,9 @@ void mainLoop(void) {
             change = derivative(controller);
 					
 					  for(int i = 0; i < 20; i++)
-					    //if(playerState.instrument == DRUMS)
-						  //  animateNote(&notes[i], &playerState, (uint16_t)100000);
-							//else 
+					    if(playerState.instrument == DRUMS)
+						    animateNote(&notes[i], &playerState, 0x0300);
+							else 
 								animateNote(&notes[i], &playerState, change);
 					
 					  updateScore(playerState.score);
